cc74eda09f5b56e825d8af2642a9eb394db13d29
Ali <ar.k@outlook.com>
2020-12-23 18:07:01 -0800

    create helper function to fetch data based on the user device or search input

--- src/App.js
+++ src/App.js
@@ -9,28 +9,63 @@ const API_KEY = "f6444799bbe49da7cf3edcbb410bc841";
 
 class App extends Component {
   
-  state={
+  state= {
+    userLocation: {
+      lat: null,
+      lon: null
+    },
     weatherData: {}
   }
 
-  getWaether = async (event) => {
-    event.preventDefault();
-    const city = event.target.elements.city.value;
-    const api_call = await fetch(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${city}`);
-    const data = await api_call.json();
-    console.log(data);
-    const weatherData = {
-      city: data.location.name,
-      country: data.location.country,
-      region: data.location.region,
-      currentDescription: data.current.weather_descriptions[0],
-      humidity: data.current.humidity,
-      windSpeed: data.current.wind_speed,
-      temperature: data.current.temperature
+  // { Fetch weather data ======================================= }
+fetchCurrentWeatherData = async (location) => {
+  const response = await fetch(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${location.coordinates1},${location.coordinates2}`);
+  const data = await response.json();
+  const weatherData = {
+    city: data.location.name,
+    country: data.location.country,
+    region: data.location.region,
+    currentDescription: data.current.weather_descriptions[0],
+    humidity: data.current.humidity,
+    windSpeed: data.current.wind_speed,
+    temperature: data.current.temperature
+  }
+  this.setState({weatherData});
+}
+
+  // { Get weather data based on the location of the user device ===================== }
+  componentDidMount() {  
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported by your browser')
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({userLocation: {
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        }})
+        // set the user location based on latitude & longitude
+        const location = {
+          coordinates1: this.state.userLocation.lat,
+          coordinates2: this.state.userLocation.lon
+        }
+        this.fetchCurrentWeatherData(location);
+      }, () => console.log('Unable to retrieve your location'))
     }
-    this.setState({weatherData});
-    // reset the search field
-    event.target.elements.city.value = "";
+  }
+
+
+// { Get weather data based on the user search ===================== }
+  getWaether = (e) => {
+    e.preventDefault();
+    // set the user location based on entered city & country input 
+    const location = {
+      coordinates1: e.target.elements.city.value,
+      coordinates2: e.target.elements.country.value
+    }
+    this.fetchCurrentWeatherData(location);
+    // clear the search field
+    e.target.elements.city.value = "";
+    e.target.elements.country.value = "";
   }
 
   render() {
--- src/components/searchField/SearchField-component.js
+++ src/components/searchField/SearchField-component.js
@@ -4,6 +4,7 @@ const SearchField = (props) => {
   return(
     <form onSubmit={props.getWaether}>
       <input type="text" placeholder="city..." name="city"/>
+      <input type="text" placeholder="country..." name="country"/>
       <button>get weather</button>
     </form>
   )
